6a40868809d6f1c01417e2bba0364ab0c71c6f88
Sharmina Akhtar <sakht318@gmail.com>
2018-08-05 17:51:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const logDriverNames = function (drivers) {
 };
 
 const logDriversByHometown = function (drivers) {
-  (drivers).forEach(function (driver,hometown){
-  if (console.log(driver.name)){
-    console.log(driver.hometown)
-    }
+  (drivers).forEach(function (driver){
+  if (driver.hometown === hometown){
+    console.log(driver.name)
+  };
   });
 };
